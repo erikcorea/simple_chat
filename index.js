d170c5f14f027e19ba74c4eba25a1ac65cbf23e1
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-04-10 13:19:14 -0400

    connecting room to fe

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const { Server } = require("socker.io");
+const { Server } = require("socket.io");
 app.use(cors());
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -14,14 +15,19 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-    console.log(socket.id);
+    console.log(`User Connected: ${socket.id}`);
 
-    socket.on("dissconnet", () => {
+    socket.on("join_room", (data) => {
+        socket.join(data)
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    })
+
+    socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
     })
 })
 
 
-server.listen(3001, ()=>{
+server.listen(3001, () => {
     console.log("server running");
 });
