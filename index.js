e4e66efd906a518e7fa3df3691a41005ea3b874d
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-04-10 12:50:08 -0400

    setting up be

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socker.io");
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("dissconnet", () => {
+        console.log("User Disconnected", socket.id);
+    })
+})
+
+
+server.listen(3001, ()=>{
+    console.log("server running");
+});
